7b39b16ede398186c80864f7150c5a98c08a9657
wizard <258075859+goon-wizard405@users.noreply.github.com>
2026-01-29 14:08:34 -0700

    Add mommy+ config script

---
+++ encourage-plus.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+
+CONF='normal'
+LEVEL='0'
+
+# usage : $0 level [conf]
+if [ $# -gt 0 ]; then
+	LEVEL=$1
+fi
+if [ $# -gt 1 ]; then
+	CONF=$2
+fi
+
+cp ~/.config/mommy/$CONF.config.sh ~/.config/mommy/config.sh
+
+aliases=(
+	curl
+	gcc
+	cbmc
+)
+for i in "${aliases[@]}"; do
+	unalias "$i" 2>/dev/null
+done
+
+
+if [ $LEVEL -eq 0 ]; then
+	echo Mommy disabled
+	# no mommy
+	if [[ -v PRE_MOMMY_PS1 ]]; then
+		PS1=$PRE_MOMMY_PS1
+	fi
+	unset PRE_MOMMY_PS1
+	if [ ! -z ${PRE_MOMMY_PROMPT_COMMAND+x} ]; then
+		PROMPT_COMMAND=$PRE_MOMMY_PROMPT_COMMAND
+	fi
+	unset PRE_MOMMY_PROMPT_COMMAND
+elif [ $LEVEL -eq 1 ]; then
+	echo Mommy on set commands
+	# set commands get upgraded
+	if [[ ! -v PRE_MOMMY_PS1 ]]; then
+		PRE_MOMMY_PS1=$PS1
+		PS1="<3 $PS1"
+	fi
+	alias curl="mommy-plus.py curl --fail-with-body"
+	alias gcc="mommy-plus.py gcc"
+	alias cbmc="mommy-plus.py cbmc"
+	if [ ! -z ${PRE_MOMMY_PROMPT_COMMAND+x} ]; then
+		PROMPT_COMMAND=$PRE_MOMMY_PROMPT_COMMAND
+	fi
+	unset PRE_MOMMY_PROMPT_COMMAND
+elif [ $LEVEL -eq 2 ]; then
+	echo Mommy only encourages
+	# no compliments, but encouragement
+	if [[ ! -v PRE_MOMMY_PS1 ]]; then
+		PRE_MOMMY_PS1=$PS1
+		PS1="<3 $PS1"
+	fi
+	echo 'MOMMY_COMPLIMENTS_ENABLED=0' >> ~/.config/mommy/config.sh
+	if [ -z ${PRE_MOMMY_PROMPT_COMMAND+x} ]; then
+		PRE_MOMMY_PROMPT_COMMAND=$PROMPT_COMMAND
+		PROMPT_COMMAND="mommy-plus.py -s \$?; $PROMPT_COMMAND"
+	fi
+else
+	echo Mommy everywhere
+	# compliments and encouragement
+	if [[ ! -v PRE_MOMMY_PS1 ]]; then
+		PRE_MOMMY_PS1=$PS1
+		PS1="<3 $PS1"
+	fi
+	if [ -z ${PRE_MOMMY_PROMPT_COMMAND+x} ]; then
+		PRE_MOMMY_PROMPT_COMMAND=$PROMPT_COMMAND
+		PROMPT_COMMAND="mommy-plus.py -s \$?; $PROMPT_COMMAND"
+	fi
+fi
+
